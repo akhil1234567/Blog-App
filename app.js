87b274582e0f55276f20f98a2eefc76557b3ea3a
akhil1234567 <akhilappu786@gmail.com>
2018-07-27 11:01:05 +0530

    Blog App

---
+++ app.js
@@ -0,0 +1,106 @@
+var express        = require('express'),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
+    mongoose       = require('mongoose'),
+    bodyParser     = require('body-parser'),
+    app            = express();
+
+mongoose.connect("mongodb://localhost/restful_blog");
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true }));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+//app.use()
+
+
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    image: String,
+    created: { type: Date, default: Date.now }
+  });
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+// Blog.create({
+//   title: "Mani",
+//   body :"Maniiide phone aayit janicha mathyayrunn",
+//   image :'http://www.clipartsuggest.com/images/46/school-golden-bell-on-white-background-royalty-free-stock-photo-y3VbpT-clipart.jpg'
+// });
+
+app.get("/", function(req,res){
+  res.redirect('/blogs')
+});
+
+app.get('/blogs',function(req,res){
+  Blog.find({},function(err , blogs){
+    if(err){
+      console.log(err);
+    }else{
+      res.render('index',{blogs: blogs})
+    }
+  })
+})
+
+app.get("/blogs/new",function(req,res){
+  res.render("new")
+})
+
+app.post("/blogs",function(req,res){
+  Blog.create(req.body.blog,function(err,newBlog){
+    if(err){
+      res.render("new");
+    }else {
+      res.redirect("blogs");
+    }
+  });
+});
+
+app.get('/blogs/:id',function(req,res){
+  Blog.findById(req.params.id,function(err,foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    }else {
+      res.render('show',{blog: foundBlog});
+    }
+  })
+})
+
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("blogs");
+    } else {
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+})
+
+app.put("/blogs/:id", function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog,function (err, updatedBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+app.delete("/blogs/:id", function(req, res){
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    }else {
+      res.redirect("/blogs");
+    }
+  })
+});
+
+
+
+app.listen(3002,function(){
+  console.log("app listening to the port 3002");
+})
